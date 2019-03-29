8aae4282e342ee3e546a2a5b1fc486a0142ba88b
codenamics <info@codenamics.pl>
2019-03-30 00:06:28 +0100

    make it bet

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let socket = require('socket.io')
 
 let app = express()
 
-let server = app.listen(5000, () => {
+let server = app.listen(process.env.PORT, () => {
     console.log('Server started')
 })
 
