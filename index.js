e6c085d43282b0fde5721547c3a8195d54ac88be
codenamics <info@codenamics.pl>
2019-03-29 23:59:19 +0100

    make it better

---
+++ index.js
@@ -0,0 +1,27 @@
+let express = require('express')
+let socket = require('socket.io')
+
+let app = express()
+
+let server = app.listen(5000, () => {
+    console.log('Server started')
+})
+
+app.use(express.static('public'))
+
+let io = socket(server)
+
+io.on('connection', (socket) => {
+    console.log('Mafe socekt connection')
+
+    socket.on('chat', (data) => {
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    socket.on('typing', function (data) {
+        console.log(data)
+        socket.broadcast.emit('typing', data);
+    });
+
+})
---
+++ public/chat.js
@@ -0,0 +1,31 @@
+// Make connection
+var socket = io.connect('http://localhost:5000');
+
+// Query DOM
+var message = document.getElementById('message'),
+    handle = document.getElementById('handle'),
+    btn = document.getElementById('send'),
+    output = document.getElementById('output'),
+    feedback = document.getElementById('feedback');
+
+// Emit events
+btn.addEventListener('click', function () {
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    });
+    message.value = "";
+});
+
+message.addEventListener('keydown', function () {
+    socket.emit('typing', handle.value);
+})
+
+socket.on('chat', function (data) {
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing', function (data) {
+    console.log(data)
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+});
